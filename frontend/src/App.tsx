ba009fd0868f65f0b8317a45e9d17104b0a7f691
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-09-03 21:16:51 -0300

    footer adicionado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Header from "./components/Header";
 import About from "./pages/About";
 import { CartProvider } from "./CartContext";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <Footer />
       </CartProvider>
     </BrowserRouter>
   );
