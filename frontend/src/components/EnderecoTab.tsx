39c1b2926b1fa26c3783fc99d5cb8707c6e4ca39
João Pedro Gonçalves <45002908+jpedro-fsgs@users.noreply.github.com>
2024-09-14 11:52:17 -0300

    feat: endereço tab

--- frontend/src/components/EnderecoTab.tsx
+++ frontend/src/components/EnderecoTab.tsx
@@ -1,10 +1,28 @@
-import { Endereco } from '@/UserContext'
-import React from 'react'
+import { Endereco } from "@/UserContext";
+import { MailIcon, MapPinIcon, PhoneIcon, UserIcon } from "lucide-react";
+import React from "react";
 
-const EnderecoTab = ({ endereco }:{endereco: Endereco | undefined}) => {
+const EnderecoTab = ({ endereco }: { endereco: Endereco | undefined }) => {
   return (
-    <div>{JSON.stringify(endereco)}</div>
-  )
-}
+    <div className="py-8 space-y-5 w-fit mx-auto mt-24">
+      {endereco ? (
+        <>
+          <div className="flex text-2xl gap-3 items-center">
+            <MapPinIcon className="size-10" />
+            Endereço:
+          </div>
+          <div className="flex text-xl gap-3 items-center pl-10">
+            Rua: {endereco.rua}, Nº: {endereco.numero}, {endereco.complemento}
+          </div>
+          <div className="flex text-xl gap-3 items-center pl-10">
+            Cidade: {endereco.cidade}, Estado: {endereco.estado}, CEP: {endereco.cep}
+          </div>
+        </>
+      ) : (
+        <div className="text-xl">Adicione um endereço</div>
+      )}
+    </div>
+  );
+};
 
-export default EnderecoTab
+export default EnderecoTab;
