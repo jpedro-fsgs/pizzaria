f02cb53da09ce09522b45536d48d64b4c37e9b00
João Pedro Gonçalves <45002908+jpedro-fsgs@users.noreply.github.com>
2024-09-16 12:39:15 -0300

    feat: botão logout

--- frontend/src/components/MeuPerfilTab.tsx
+++ frontend/src/components/MeuPerfilTab.tsx
@@ -1,17 +1,32 @@
-import { UserContextType } from "@/UserContext"
+import { UserContextType } from "@/UserContext";
 import { MailIcon, PhoneIcon, UserIcon } from "lucide-react";
 import { Button } from "./ui/button";
 
-
-const MeuPerfilTab = ({ user }:{user: UserContextType}) => {
-  const { id, nome, telefone, endereco, email } = user;
+const MeuPerfilTab = ({ user }: { user: UserContextType }) => {
+  const { id, nome, telefone, email, logout } = user;
   return (
-    <div className="py-8 space-y-5 w-fit mx-auto mt-24">
-      <div className="flex text-2xl gap-3 items-center"><UserIcon className="size-10" />Nome: {nome}</div>
-      <div className="flex text-2xl gap-3 items-center"><PhoneIcon className="size-10" />Telefone: {telefone}</div>
-      <div className="flex text-2xl gap-3 items-center"><MailIcon className="size-10" />Email: {email}</div>
-    </div>
-  )
-}
+    <>
+      <div className="py-8 space-y-5 w-fit mx-auto mt-24">
+        <div className="flex text-2xl gap-3 items-center">
+          <UserIcon className="size-10" />
+          Nome: {nome}
+        </div>
+        <div className="flex text-2xl gap-3 items-center">
+          <PhoneIcon className="size-10" />
+          Telefone: {telefone}
+        </div>
+        <div className="flex text-2xl gap-3 items-center">
+          <MailIcon className="size-10" />
+          Email: {email}
+        </div>
+      </div>
+      <div className="flex">
+        <Button className="mx-auto" onClick={logout}>
+          Logout
+        </Button>
+      </div>
+    </>
+  );
+};
 
-export default MeuPerfilTab
+export default MeuPerfilTab;
