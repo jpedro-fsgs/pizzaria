c4cfeed8cf26fe3e1d6d2ea1e347b092bf9215df
João Pedro Gonçalves <45002908+jpedro-fsgs@users.noreply.github.com>
2024-09-23 16:13:02 -0300

    feat: rota de envio de email

---
+++ backend/app/email.py
@@ -0,0 +1,160 @@
+import os
+from fastapi import HTTPException, status
+from email.message import EmailMessage
+import aiosmtplib
+
+from models.pedidos import PedidoResponse
+
+EMAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
+EMAIL_SENHA = os.environ.get("EMAIL_SENHA")
+EMAIL_HOSTNAME = os.environ.get("EMAIL_HOSTNAME")
+
+def criar_email(pedido: PedidoResponse, chave_pix: str):
+    return f'''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Confirmação de Pedido</title>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+            <style>
+                body {{
+                    font-family: 'Poppins', Arial, sans-serif;
+                    background-color: #e5e7eb;
+                    color: #111827;
+                    margin: 0;
+                    padding: 0;
+                }}
+                .container {{
+                    background-color: #ffffff;
+                    max-width: 600px;
+                    margin: 20px auto;
+                    border: 1px solid #ddd;
+                    border-radius: 8px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    overflow: hidden;
+                }}
+                .header {{
+                    background-color: #E7191F;
+                    color: white;
+                    padding: 20px;
+                    text-align: center;
+                }}
+                .header h1 {{
+                    margin: 0;
+                    font-size: 24px;
+                    font-weight: 600;
+                }}
+                .content {{
+                    padding: 20px;
+                }}
+                .content h2 {{
+                    color: #E7191F;
+                    font-size: 20px;
+                    font-weight: 600;
+                    margin-bottom: 10px;
+                }}
+                .product {{
+                    border-bottom: 1px solid #ddd;
+                    padding-bottom: 10px;
+                    margin-bottom: 10px;
+                }}
+                .product p {{
+                    margin: 5px 0;
+                    color: #111827;
+                }}
+                .adicionais {{
+                    color: #fb923c;
+                }}
+                .pix-info {{
+                    margin-top: 20px;
+                    padding: 10px;
+                    background-color: #facc15;
+                    border-radius: 8px;
+                    text-align: center;
+                }}
+                .pix-info p {{
+                    margin: 10px 0;
+                    color: #111827;
+                    font-size: 16px;
+                    font-weight: 600;
+                    word-break: break-all;
+                }}
+                .pix-info .total {{
+                    font-size: 18px;
+                    color: #22c55e;
+                }}
+                .pix-info img {{
+                    margin-top: 10px;
+                    width: 150px;
+                    height: 150px;
+                    border: 1px solid #111827;
+                    border-radius: 4px;
+                }}
+                .footer {{
+                    background-color: #facc15;
+                    padding: 10px;
+                    text-align: center;
+                    font-size: 14px;
+                    color: #111827;
+                }}
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <h1>Pedido Confirmado!</h1>
+                </div>
+                <div class="content">
+                    <h2>Olá, {pedido.nome_usuario}</h2>
+                    <p>Seu pedido n°<strong>{pedido.id}</strong> foi confirmado em {pedido.horario_pedido.strftime('%d/%m/%Y %H:%M')}.</p>
+                    <div>
+                        {''.join([f'''
+                            <div class="product">
+                                <p><strong>Produto:</strong> {produto.nome_produto} {f"({produto.tamanho})" if produto.tamanho else ""}</p>
+                                <p><strong>Quantidade:</strong> {produto.quantidade}</p>
+                                <p><strong>Preço:</strong> R$ {produto.preco:.2f}</p>
+                                {'<p class="adicionais"><strong>Adicionais:</strong></p><ul>' + ''.join([f'<li>{adicional}</li>' for adicional in produto.adicionais]) + '</ul>' if produto.adicionais else ''}
+                            </div>
+                        ''' for produto in pedido.produtos])}
+                    </div>
+                    <div class="pix-info">
+                        <p>Valor total: <span class="total">R$ {pedido.total:.2f}</span></p>
+                        <p>Chave PIX: <strong>{chave_pix.payload}</strong></p>
+                    </div>
+                </div>
+                <div class="footer">
+                    <p>Agradecemos por escolher a nossa pizzaria!</p>
+                </div>
+            </div>
+        </body>
+        </html>
+    '''
+
+
+
+async def send_email(subject: str, body: str, to: str, fallback: str):
+    # Criação da mensagem de e-mail
+    message = EmailMessage()
+    message["From"] = f"Pizzaria Borcelle<{EMAIL_USERNAME}>"
+    message["To"] = to
+    message["Subject"] = subject
+    message.set_content(fallback)
+    message.add_alternative(body, subtype="html")
+    
+    # Envio do e-mail
+    
+    try:
+        response = await aiosmtplib.send(
+            message,
+            hostname=EMAIL_HOSTNAME,  # Coloque o servidor SMTP
+            port=587,
+            username=EMAIL_USERNAME,
+            password=EMAIL_SENHA,
+            start_tls=True,
+        )
+        return response
+
+    except aiosmtplib.SMTPException as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Falha no envio do e-mail: {str(e)}")
--- backend/routes/pedidos.py
+++ backend/routes/pedidos.py
@@ -2,8 +2,10 @@ import os
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import HTMLResponse
 from sqlalchemy import desc
 
+from app.email import criar_email, send_email
 from database.schema import (
     Pedido,
     PedidoProduto,
@@ -38,7 +40,12 @@ async def get_pedidos_usuario(
     session: Session = Depends(get_session),
 ):
 
-    pedidos = session.query(Pedido).filter(Pedido.id_usuario == user["id"]).order_by(desc(Pedido.horario_pedido)).all()
+    pedidos = (
+        session.query(Pedido)
+        .filter(Pedido.id_usuario == user["id"])
+        .order_by(desc(Pedido.horario_pedido))
+        .all()
+    )
     pedido_produtos = session.query(PedidoProduto)
 
     pedido_response = []
@@ -47,12 +54,11 @@ async def get_pedidos_usuario(
         for produto in pedido.produtos:
 
             produtos_pedido = (
-                pedido_produtos
-                .filter(PedidoProduto.pedido_id == pedido.id)
+                pedido_produtos.filter(PedidoProduto.pedido_id == pedido.id)
                 .filter(PedidoProduto.produto_id == produto.id)
                 .all()
             )
-            
+
             for produto_pedido in produtos_pedido:
                 produtos.append(
                     ProdutoPedidoResponse(
@@ -77,48 +83,23 @@ async def get_pedidos_usuario(
     return pedido_response
 
 
-@router.post("/cadastrar/", response_model=PedidoResponse)
-async def cadastrar_pedido(
-    pedido_input: RealizarPedido,
-    user: Annotated[dict, Depends(get_current_usuario)],
+@router.get("/{pedido_id}/", response_model=PedidoResponse)
+async def get_pedido(
+    pedido_id: int,
+    # user: Annotated[dict, Depends(get_current_usuario)],
     session: Session = Depends(get_session),
 ):
-
-    pedido = Pedido(id_usuario=user["id"], total=0)
-    session.add(pedido)
-    session.flush()
-    produtos = []
-    total = 0
-
-    for produto_input in pedido_input.produtos:
-        produto = session.query(Produto).get(produto_input.id_produto)
-
-        if not produto:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado")
-
-        produtos.append(produto)
-        total += produto_input.preco * produto_input.quantidade
-        session.add(
-            PedidoProduto(
-                pedido_id=pedido.id,
-                adicionais_pedido=produto_input.adicionais,
-                produto_id=produto.id,
-                tamanho=produto_input.tamanho,
-                quantidade=produto_input.quantidade,
-                preco_total=produto_input.preco * produto_input.quantidade,
-            )
+    pedido = session.query(Pedido).get(pedido_id)
+    if not pedido:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Pedido não encontrado"
         )
-
-    pedido.total = total
-    session.add(pedido)
-    session.commit()
 
     produtos = []
     for produto in pedido.produtos:
         pedido_produtos = session.query(PedidoProduto)
         produtos_pedido = (
-            pedido_produtos
-            .filter(PedidoProduto.pedido_id == pedido.id)
+            pedido_produtos.filter(PedidoProduto.pedido_id == pedido.id)
             .filter(PedidoProduto.produto_id == produto.id)
             .all()
         )
@@ -142,7 +123,74 @@ async def cadastrar_pedido(
         horario_pedido=pedido.horario_pedido,
         total=pedido.total,
     )
-        
+
+
+@router.post("/cadastrar/", response_model=PedidoResponse)
+async def cadastrar_pedido(
+    pedido_input: RealizarPedido,
+    user: Annotated[dict, Depends(get_current_usuario)],
+    session: Session = Depends(get_session),
+):
+
+    pedido = Pedido(id_usuario=user["id"], total=0)
+    session.add(pedido)
+    session.flush()
+    produtos = []
+    total = 0
+
+    for produto_input in pedido_input.produtos:
+        produto = session.query(Produto).get(produto_input.id_produto)
+
+        if not produto:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado"
+            )
+
+        produtos.append(produto)
+        total += produto_input.preco * produto_input.quantidade
+        session.add(
+            PedidoProduto(
+                pedido_id=pedido.id,
+                adicionais_pedido=produto_input.adicionais,
+                produto_id=produto.id,
+                tamanho=produto_input.tamanho,
+                quantidade=produto_input.quantidade,
+                preco_total=produto_input.preco * produto_input.quantidade,
+            )
+        )
+
+    pedido.total = total
+    session.add(pedido)
+    session.commit()
+
+    produtos = []
+    for produto in pedido.produtos:
+        pedido_produtos = session.query(PedidoProduto)
+        produtos_pedido = (
+            pedido_produtos.filter(PedidoProduto.pedido_id == pedido.id)
+            .filter(PedidoProduto.produto_id == produto.id)
+            .all()
+        )
+        for produto_pedido in produtos_pedido:
+            produtos.append(
+                ProdutoPedidoResponse(
+                    id_produto=produto.id,
+                    nome_produto=produto.nome,
+                    quantidade=produto_pedido.quantidade,
+                    preco=produto_pedido.preco_total,
+                    adicionais=produto_pedido.adicionais_pedido,
+                    tamanho=produto_pedido.tamanho,
+                )
+            )
+
+    return PedidoResponse(
+        id=pedido.id,
+        id_usuario=pedido.id_usuario,
+        nome_usuario=pedido.usuario.nome,
+        produtos=produtos,
+        horario_pedido=pedido.horario_pedido,
+        total=pedido.total,
+    )
 
 
 @router.get("/pix/{pedido_id}/", response_model=PayloadPix)
@@ -150,8 +198,39 @@ async def gerar_pix(pedido_id: int, session: Session = Depends(get_session)):
     pedido = session.query(Pedido).get(pedido_id)
 
     if not pedido:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pedido não encontrado")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Pedido não encontrado"
+        )
 
     payload_pix = Payload(CHAVE_PIX, pedido.total).gerarPayload()
 
     return PayloadPix(payload=payload_pix)
+
+
+@router.post("/email/{pedido_id}/")
+async def enviar_email(
+    pedido_id: int,
+    user: Annotated[dict, Depends(get_current_usuario)],
+    session: Session = Depends(get_session),
+):
+    pedido = session.query(Pedido).get(pedido_id)
+
+    if not pedido:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    if pedido.usuario.id != user["id"]:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    subject = f"Pizzaria Borcelle: Pedido n°{pedido.id} confirmado"
+    body = criar_email(
+        await get_pedido(pedido.id, session), await gerar_pix(pedido.id, session)
+    )
+    to = pedido.usuario.email
+    fallback = f"Pizzaria Borcelle: Pedido n°{pedido.id} de {pedido.usuario.nome} foi confirmado"
+
+    response = await send_email(subject, body, to, fallback)
+
+    return {
+        "message": f"E-mail enviado para {pedido.usuario.email}",
+        "response": response,
+    }
